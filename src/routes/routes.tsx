ccb4114a355b6d071430ec7c2265bcfd36525a4e
Leonardo Alves <leonardo1029leonardo1029@gmail.com>
2026-09-22 21:51:05 -0300

    feat: autenticação com telas de login e registro

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,8 @@ import App from '../App';
 import { Home } from '../pages/Home/Home';
 import { PlayerDetail } from '../pages/PlayerDetail/PlayerDetail';
 import { NotFound } from '../pages/NotFound/NotFound';
+import { Login } from '../pages/Auth/Login';
+import { Register } from '../pages/Auth/Register';
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +13,8 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'player/:id', element: <PlayerDetail /> },
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
       { path: '*', element: <NotFound /> },
     ],
   },
